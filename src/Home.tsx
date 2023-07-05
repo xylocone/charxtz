2d8d38d73f2b18380a7f6e01b2dc21d13bf1e4c2
Arjun Singh <singharjun.biz@gmail.com>
2023-07-05 10:21:38 +0530

    feat: present data in a stylized manner

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 // Internal dependencies
 import type { ContractStorage, CharityComponentProps } from "./types";
-import { getStorage, donate } from "./api";
+import { getStorage, donate, getConnectedWalletAccountAddress } from "./api";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
@@ -37,58 +37,116 @@ const HomePage = () => {
   );
 };
 
+// Utility function to truncate string to desired number of characters
+function truncate(str: string, n: number) {
+  return str.length > n ? str.slice(0, n - 1) + "..." : str;
+}
+
+// Utility function to copy to clipboard
+function copyToClipboard(text: string) {
+  navigator.clipboard.writeText(text);
+}
+
 export function CharityComponent(props: CharityComponentProps) {
   const amountRef = useRef<HTMLInputElement>(null);
   let { title, target, donations, total_fund, timestamp, charity } =
     props.storage;
 
-  // Mock values
-
-  title =
-    "Charity drive for providing wheelchairs to specially-abled people in Kamla Nagar";
-  target = 2500;
-  (donations = {}),
-    (total_fund = 500),
-    (timestamp = 18451781118),
-    (charity = "0x189189de23rjo");
-
-  donations;
+  total_fund = total_fund / 1e6; // convert mutez to tez
+  target = target / 1e6; // convert mutez to tex
 
   const handleDonation = async () => {
-    if (amountRef.current) await donate(parseInt(amountRef.current.value, 10));
+    if (amountRef.current) {
+      const amountToDonate = parseInt(amountRef.current.value, 10);
+      if (await getConnectedWalletAccountAddress()) {
+        console.log(await getConnectedWalletAccountAddress());
+        if (amountToDonate > 0) {
+          await donate(amountToDonate);
+          window.location.reload();
+        } else alert("You need to enter a valid donation amount.");
+      } else alert("You need to connect your wallet first.");
+    }
   };
 
   return (
     <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 10, opacity: 0 }}>
-      <Card className="my-4">
+      <Card className="my-4 border-2 md:max-w-[80vw] m-auto">
         <div>
           <Badge color="purple" className="px-2 w-fit rounded-xl">
-            {new Date(timestamp * 1000).toLocaleDateString("en-us", {
+            {new Date(timestamp).toLocaleDateString("en-us", {
               year: "numeric",
               month: "long",
               day: "numeric",
             })}
           </Badge>
           <h3 className="font-semibold text-[1.5rem]">{title}</h3>
-          <p className="my-4 text-lg font-medium">
-            <span className="text-gray">Charity Address: </span> {charity}
+          <p className="my-4 text-sm font-medium">
+            <span className="text-slate-900">Charity Address: </span>
+            <span
+              className="text-ellipsis"
+              onClick={() => copyToClipboard(charity)}
+            >
+              {truncate(charity, 20)}
+            </span>
           </p>
           <div>
-            <Progress color="purple" progress={(total_fund / target) * 100} />
+            <div>
+              <Progress color="purple" progress={(total_fund / target) * 100} />
+              <p className="text-sm font-semibold">
+                {Math.min(total_fund, target)} XTZ
+                <span className="text-slate-900"> out of </span> {target} XTZ
+                <span className="text-slate-900"> collected</span>
+              </p>
+            </div>
           </div>
           <div className="flex gap-2 my-4">
             <TextInput
               ref={amountRef}
-              id="amount"
-              placeholder="20"
+              placeholder="100"
               required
               type="number"
+              className="basis-0 grow-[8]"
             />
-            <Button onClick={async () => await handleDonation()}>Donate</Button>
+            <Button
+              className="basis-0 grow-[4] uppercase"
+              onClick={async () => await handleDonation()}
+              gradientDuoTone="purpleToBlue"
+            >
+              Donate
+            </Button>
           </div>
+          <Donations donations={donations} />
         </div>
       </Card>
     </motion.div>
+  );
+}
+
+interface DonationsProps {
+  donations: { [key: string]: number };
+}
+
+function Donations(props: DonationsProps) {
+  return (
+    <div>
+      <hr />
+      <h3 className="my-2 font-medium uppercase text-md">Donors </h3>
+      <div className="max-h-[200px] overflow-auto">
+        {Object.keys(props.donations).map((addr) => (
+          <div key={addr} className="flex items-center gap-4">
+            <Badge
+              className="my-1 p-2 basis-0 grow-[8]"
+              onClick={() => copyToClipboard(addr)}
+            >
+              {truncate(addr, 20)}
+            </Badge>
+            <span className="inline-block basis-0 grow-[4] text-right text-sm font-semibold">
+              {props.donations[addr] / 1e6} XTZ
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
