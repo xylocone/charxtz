b9789e92b334d78e805a60303c9bec9224b18061
Arjun Singh <singharjun.biz@gmail.com>
2023-07-05 06:54:14 +0530

    feat: update SmartPy contract

--- backend/contract.py
+++ backend/contract.py
@@ -1,38 +1,38 @@
-# A very simple SmartContract that will keep track of the XTZs sent to it
+# A very simple SmartContract that will keep track of the XTZs sent into it and the addresses of
+# the people who have sent them
 
 import smartpy as sp
 
-
 @sp.module
 def main():
-    params_type: type = sp.record(adminAddress=sp.address)
-
     class CharityContract(sp.Contract):
         def __init__(self):
+            self.data.hasSetAdmin = False
             self.data.title = ""
-            self.data.admin = sp.address("")
+            self.data.admin = sp.self_address()
             self.data.target = sp.tez(0)
             self.data.donations = {}
             self.data.total_fund = sp.tez(0)
             self.data.timestamp = sp.now
-            self.data.charity = sp.address("")
-
+            self.data.charity = sp.self_address()
+    
         @sp.entrypoint
         def donate(self):
             assert sp.amount > sp.tez(0)
             self.data.total_fund += sp.amount
-
+            
             self.data.donations[sp.sender] = sp.amount
 
             if self.data.total_fund >= self.data.target:
                 sp.send(self.data.charity, self.data.total_fund)
 
         @sp.entrypoint
-        def setAdmin(self, admin):
+        def setAdmin(self, params):
             # Immediately call this entrypoint after SmartContract deployment to gain
             # administrator access to the SmartContract
-            assert self.data.admin == sp.address("")
-            self.data.admin = admin
+            assert not self.data.hasSetAdmin
+            self.data.admin = params.new_admin
+            self.data.hasSetAdmin = True
 
         @sp.entrypoint
         def setFields(self, params):
@@ -41,8 +41,7 @@ def main():
             self.data.target = params.target
             self.data.timestamp = params.timestamp
             self.data.charity = params.charity
-
-
+    
 @sp.add_test(name="test")
 def test():
     scenario = sp.test_scenario()
@@ -59,11 +58,7 @@ def test():
 
     # change_num_values
     scenario.h2("CharityContract Test1")
-    contract.setAdmin(admin.address)
-    contract.setFields(
-        title="Team Ehsas Charity",
-        target=sp.tez(5000),
-        timestamp=sp.now,
-        charity=charity.address,
-    ).run(sender=admin)
+    contract.setAdmin(new_admin=admin.address)
+    contract.setFields(title="Team Ehsas Charity", target=sp.tez(5000), timestamp=sp.now, charity=charity.address).run(sender=admin)
     contract.donate().run(sender=alice, amount=sp.tez(15))
+            
