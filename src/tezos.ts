dcc816843af03c770c4529177c0781233607cc7c
Arjun Singh <singharjun.biz@gmail.com>
2023-07-05 08:05:05 +0530

    fix: update contract link and network type

--- src/tezos.ts
+++ src/tezos.ts
@@ -2,19 +2,23 @@ import { TezosToolkit } from "@taquito/taquito";
 import { BeaconWallet } from "@taquito/beacon-wallet";
 import { NetworkType } from "@airgap/beacon-sdk";
 
-const CONTRACT_ADDRESS = "KT1Gz2Fz5Y8U3WQ2kM4y9giNEveYVx34rt2T";
+const CONTRACT_ADDRESS = "KT1W1xZqZfaMRpR1cd17yVzsKhLgHwepYL9q";
 
 const wallet = new BeaconWallet({
   name: "CharXTZ",
   preferredNetwork: NetworkType.GHOSTNET,
 });
 
-const tezos = new TezosToolkit("https://ghostnet.smartpy.io/");
+const tezos = new TezosToolkit("https://ghostnet.smartpy.io");
 tezos.setWalletProvider(wallet);
 
 // Asynchronous function that pops up the Beacon SDK UI to connect the app to the user's wallet
 const connectWallet = async () => {
-  await wallet.requestPermissions();
+  await wallet.requestPermissions({
+    network: {
+      type: NetworkType.GHOSTNET,
+    },
+  });
 };
 
 // Asynchronous function to get the address of the user's connected account
