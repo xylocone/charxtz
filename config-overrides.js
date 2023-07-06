c23c458dd455db6ca8bfdef8c79fb0778d212e1c
Arjun Singh <singharjun.biz@gmail.com>
2023-07-06 10:12:59 +0530

    fix: remove the ModuleScopePlugin that fails the Vercel build

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,8 @@
-// config-overrides.js
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 module.exports = function override(config, env) {
+    config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
     config.plugins.push(new NodePolyfillPlugin())
     return config
 }
